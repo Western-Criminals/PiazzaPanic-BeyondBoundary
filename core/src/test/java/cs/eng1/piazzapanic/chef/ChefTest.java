ab185e7f04603611db6cf7b40f259ae46052d99e
ell-wh3345 <117932005+ell-wh3345@users.noreply.github.com>
2023-04-25 17:05:12 +0100

    i think this should be the right infrastructure for testing

---
+++ core/src/test/java/cs/eng1/piazzapanic/chef/ChefTest.java
@@ -0,0 +1,11 @@
+package cs.eng1.piazzapanic.chef;
+
+import junit.framework.TestCase;
+
+public class ChefTest extends TestCase {
+
+    public void testHasIngredient() {
+
+
+    }
+}
---
+++ core/src/test/java/cs/eng1/piazzapanic/food/recipes/RecipeTest.java
@@ -0,0 +1,18 @@
+package cs.eng1.piazzapanic.food.recipes;
+
+import junit.framework.TestCase;
+
+public class RecipeTest extends TestCase {
+
+
+    public void testGetTexture() {
+
+    }
+
+    public void testGetType() {
+    }
+
+    public void testGetRecipeIngredients() {
+
+    }
+}
